81da7db77f64f3ec447e704f25853ed4f011e406
Jonathan Caburnay <jmcaburnay1@up.edu.ph>
2022-05-16 21:27:09 +0800

    Exercise 4.11*: Blog list tests, step4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -34,25 +34,45 @@ test('identifier property is id not _id', async () => {
   expect(result[0]).toBeDefined()
 })
 
-test('addition of new blog', async () => {
-  const newBlog = {
-    title: 'Me, a former physicist, just launched my first project using Elixir and Phoenix.',
-    author: 'Felipe Lincoln',
-    url: 'https://dev.to/felipelincoln/me-a-former-physicist-just-launched-my-first-project-using-elixir-and-phoenix-8k',
-    likes: 2
-  }
+describe('addition of a new blog', () => {
+  test('addition of new blog', async () => {
+    const newBlog = {
+      title: 'Me, a former physicist, just launched my first project using Elixir and Phoenix.',
+      author: 'Felipe Lincoln',
+      url: 'https://dev.to/felipelincoln/me-a-former-physicist-just-launched-my-first-project-using-elixir-and-phoenix-8k',
+      likes: 2
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const blogs = await helper.blogsInDb()
-  expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+    const blogs = await helper.blogsInDb()
+    expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
 
-  const titles = blogs.map((n) => n.title)
-  expect(titles).toContain('Me, a former physicist, just launched my first project using Elixir and Phoenix.')
+    const titles = blogs.map((n) => n.title)
+    expect(titles).toContain('Me, a former physicist, just launched my first project using Elixir and Phoenix.')
+  })
+
+  test('no likes property in request', async () => {
+    const newBlog = {
+      title: 'New Blog - Full Stack Open 2022 Part 4',
+      author: 'Jonathan Caburnay',
+      url: 'https://fullstackopen.com/en/part4/testing_the_backend#exercises-4-8-4-12'
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogs = await helper.blogsInDb()
+    expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+    const newlyAddedBlog = blogs.find(blog => blog.title === 'New Blog - Full Stack Open 2022 Part 4')
+    expect(newlyAddedBlog.likes).toBe(0)
+  })
 })
 
 afterAll(() => {
