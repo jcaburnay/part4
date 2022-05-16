fa923426698798d04dcdcd8acc4822494b6c2867
Jonathan Caburnay <jcaburnay@stratpoint.com>
2022-05-16 14:35:40 +0800

    Exercise 4.8 Blog list tests, step1

---
+++ tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Understanding how API routes work in Next.js',
+    author: 'Cody Jarrett',
+    url: 'https://dev.to/codymjarrett/understanding-how-api-routes-work-in-next-js-50fm',
+    likes: 4
+  },
+  {
+    title: 'Website Setup Essentials',
+    author: 'PRIM4T',
+    url: 'https://dev.to/prim4t/website-setup-essentials-447o',
+    likes: 3
+  },
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ content: 'willremovethissoon', date: new Date() })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(note => note.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
